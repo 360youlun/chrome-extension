226d5246d999fd6bcbfc03c98b9111b0938518b4
maxprychynenko <maxprychynenko@gmail.com>
2016-06-27 14:23:39 +0300

    Convert any link to ERP in custom fields section

--- tpondemand/extension.js
+++ tpondemand/extension.js
@@ -1,47 +1,15 @@
 (function() {
-    var STATE_HANDLERS = {
-        'other': function() {},
-        'userstory': updateUserStory,
-    };
-
     var interval = setInterval(tick, 1000);
 
     function tick() {
-        var state = getState();
-        STATE_HANDLERS[state]();
+        handleErpLinks();
     }
 
-    function getRoute() {
-        var components = window.location.hash.substr(1).split('&');
-        var route = {};
-        for (var i = 0, len = components.length; i < len; i++) {
-            components[i] = components[i].split('=', 2);
-            route[components[i][0]] = components[i][1];
-        }
-        return route;
+    function handleErpLinks() {
+        $('.ui-customfield__value:contains(erp.ylly.com/)').each(convertUrlToLink);
     }
 
-    function getState() {
-        var route = getRoute();
-        if (route.boardPopup) {
-            if (route.boardPopup.indexOf('userstory/') === 0) {
-                return 'userstory';
-            }
-        } else if (route.page) {
-            if (route.page.indexOf('userstory/') === 0) {
-                return 'userstory';
-            }
-        } else {
-            return 'other';
-        }
-    }
-
-    function updateUserStory() {
-        $('.ui-customfield__value:contains(erp.ylly.com/team/stories)')
-            .each(updateErpField);
-    }
-
-    function updateErpField(index, element) {
+    function convertUrlToLink(index, element) {
         element = $(element);
 
         if (element.children().length > 0) {
